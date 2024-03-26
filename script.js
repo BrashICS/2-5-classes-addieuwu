9fb907a29c1acf421ca2b7ae6dc6884f80daaacf
Addie <isaac.de.boer@stu.ocsb.ca>
2024-03-26 09:03:19 -0400

    update

--- script.js
+++ script.js
@@ -11,4 +11,46 @@
 
 'use strict';
 
+class Circle {
+  name = "Circle";
+  radius = 1;
 
+  area () {
+    return this.radius * Math.PI ** 2;
+  }
+
+  circumference () {
+    return 2 * this.radius * Math.PI;
+  }
+}
+
+class Rectangle {
+  name = "Rectangle";
+  length = 1;
+  width = 1;
+
+  
+}
+
+class Vehicle {
+  type;
+  num_wheels;
+  num_doors;
+  num_seats;
+  fuel;
+  can_fly = false;
+}
+
+
+
+function tankGenerator() {
+  let tank = new Vehicle;
+  tank.type = "AFV"
+  tank.num_wheels = 14;
+  tank.num_doors = 3;
+  tank.num_seats = 4;
+  tank.fuel = "D12A1 Diesel"
+  return tank;
+}
+
+console.log(tankGenerator());
